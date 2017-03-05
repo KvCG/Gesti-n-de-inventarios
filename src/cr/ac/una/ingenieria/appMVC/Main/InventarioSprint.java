c942a7026b54a96dc3b480f4c08ce4bcb25649ee
Michael <Michael@DESKTOP-3Q6TQ54>
2017-03-04 21:37:51 -0600

    Cambio pantalla login

--- src/cr/ac/una/ingenieria/appMVC/Main/InventarioSprint.java
+++ src/cr/ac/una/ingenieria/appMVC/Main/InventarioSprint.java
@@ -5,10 +5,6 @@
  */
 package cr.ac.una.ingenieria.appMVC.Main;
 
-import cr.ac.una.ingenieria.appMVC.BL.ArticuloBL;
-import cr.ac.una.ingenieria.appMVC.BL.ProveedorBL;
-import cr.ac.una.ingenieria.appMVC.BL.UsuarioBL;
-import cr.ac.una.ingenieria.appMVC.Controlador.PantallaPrincipalControlador;
 import cr.ac.una.ingenieria.appMVC.Controlador.ValidarAcceso;
 import cr.ac.una.ingenieria.appMVC.Vista.PantallaPrincipal;
 import cr.ac.una.ingenieria.appMVC.Vista.SolicitudUsuarioPassword;
@@ -23,12 +19,7 @@ public class InventarioSprint {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-//        PantallaPrincipal pantPrinView = new PantallaPrincipal();
-//        UsuarioBL usuBL = new UsuarioBL();
-//        ArticuloBL articuloBlModelo= new ArticuloBL();
-//        ProveedorBL proveedorBlModelo= new ProveedorBL();
-//        PantallaPrincipalControlador ppC = new PantallaPrincipalControlador(pantPrinView, usuBL,proveedorBlModelo,articuloBlModelo);
-//        ppC.getPantPrinView().setVisible(true);
+
         
         SolicitudUsuarioPassword solUsuarioPassword =new SolicitudUsuarioPassword();
         PantallaPrincipal pantPrinView = new PantallaPrincipal();
